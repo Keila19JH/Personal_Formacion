ef8c7709ee72a280f0f2b15f904ba8112305743b
Keila19JH <keila19gutierrez@gmail.com>
2025-01-14 08:58:21 -0600

    fist commit

---
+++ js/plugins/loader.plugin.js
@@ -0,0 +1,9 @@
+
+export function showLoadingOverlay() {
+    document.getElementById('loading-overlay').style.display = 'flex';
+}
+
+export function hideLoadingOverlay() {
+    document.getElementById('loading-overlay').style.display = 'none';
+}
+
